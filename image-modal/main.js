bc2696790ff45b307d74272dd2545b361ef54f47
Katherine <kbd3ax@virginia.edu>
2017-08-02 12:46:04 -0400

    modal calculates height based on image in div

--- image-modal/main.js
+++ image-modal/main.js
@@ -1,6 +1,8 @@
-var modal = document.getElementById('myModal');
-var btn = document.getElementById('myBtn');
+var modal = document.getElementById('myModal'); // container
+var btn = document.getElementById('myBtn'); // button
 var span = document.getElementsByClassName('close')[0];
+
+// open/close
 
 btn.addEventListener('click', function() {
   modal.style.display = "block";
@@ -11,9 +13,16 @@ span.addEventListener('click', function() {
 });
 
 function closeModal(event) {
-  if (event.target == modal) {
+  if (event.target == modal) { // height/width = 100%
     modal.style.display = "none";
   }
 }
-
 window.addEventListener('click', closeModal);
+
+// generate content
+
+var pic = document.getElementsByClassName('pic')[0];
+var container = document.getElementsByClassName('modal-content')[0];
+var height = window.getComputedStyle(pic).getPropertyValue('height');
+container.style.height = height;
+console.log(height);
